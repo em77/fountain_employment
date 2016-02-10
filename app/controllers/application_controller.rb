30489cda369d086e281a8cb2c427545bee49f358
Eric M <ericmarchese77@gmail.com>
2016-02-10 00:53:34 -0500

    Fixed improper ordering with new date format

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,16 @@ class ApplicationController < ActionController::Base
     to_paginate.paginate(page: params[:page], per_page: num_per_page)
   end
 
+  def date_order_formatter(date_string)
+    Date.strptime(date_string,"%m-%d-%Y").to_s
+  end
+
   def orderer(result, order_by, order_direction = "asc")
     if order_by == "company_id"
       result_ordered = result.joins(:company).order("companies.name asc")
+    elsif order_by == "start_date" || order_by == "est_end_date"
+      result_ordered = result.sort_by{|listing| date_order_formatter(listing.send(order_by.to_sym))}
+      result_ordered = result_ordered.reverse if order_direction == "desc"
     else
       result_ordered = result.order("#{order_by} #{order_direction}")
     end
